44339ed1999b8ed26fa61cf1a446f96b29f84310
Lamine Diaw <laminediaw111@gmail.com>
2025-06-05 16:15:17 +0200

    freenlance in the beningingz

---
+++ mvp/mvp/src/test/java/rate_calculator/mvp/BackendRateCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package rate_calculator.mvp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendRateCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
